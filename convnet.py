b0434e0dae307ee2077f784d5e56add5e3440faa
Yiling Chen <balling.xo@gmail.com>
2019-03-02 16:05:12 -0800

    set random seed and remove dropout

--- convnet.py
+++ convnet.py
@@ -7,6 +7,10 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.datasets import mnist
+from numpy.random import seed
+from tensorflow import set_random_seed
+seed(42)
+set_random_seed(42)
 
 NUM_IMG = 25
 
@@ -46,7 +50,7 @@ model = Sequential()
 model.add(Conv2D(4, (3, 3), strides=(2,2), activation='relu', input_shape=(1024, 1024, 1), data_format='channels_last'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(4, (3, 3), strides=(2,2), activation='relu'))
-model.add(Dropout(0.25))
+# model.add(Dropout(0.25))
  
 model.add(Flatten())
 model.add(Dense(1024, activation='relu'))
